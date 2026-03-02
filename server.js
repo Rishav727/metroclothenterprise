55af866c4f6abe839458b993efc3757fd9f3038e
Rishav727 <joypaul727856@gmail.com>
2026-03-02 22:21:52 +0530

    path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(express.static('public'));
+app.use(cors());
 
 // Temporary storage (In a real app, use MongoDB)
 let orders = [];
